be7010f261ca38391de02aeeff947aaf0a4b4925
Alejandro <alejandro@mac-mini.local>
2016-02-15 23:40:16 +0100

    Configuración de autoretardo y cabecera precompilada

---
+++ Everpobre/Settings.h
@@ -0,0 +1,10 @@
+//
+//  Settings.h
+//  Everpobre
+//
+//  Created by Alejandro on 15/2/16.
+//  Copyright © 2016 Alejandro. All rights reserved.
+//
+
+#define AUTO_SAVE NO
+#define AUTO_SAVE_DELAY_IN_SECONDS 30
